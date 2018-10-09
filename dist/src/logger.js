b9088410de3ab645e20738c7ec22a289061c3ba1
thec00n <thec00n@fork.at>
2018-10-09 16:36:17 +0800

    remove dist

--- dist/src/logger.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Logger = require("logplease");
-Logger.setLogLevel("DEBUG");
-exports.default = Logger.create("utils");
