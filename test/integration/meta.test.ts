4e012f967230e5d834fa1a8acd5817b96ccb96ae
thec00n <thec00n@fork.at>
2018-11-21 21:36:14 +0800

    update circleci

--- test/integration/meta.test.ts
+++ test/integration/meta.test.ts
@@ -1,11 +1,11 @@
 
 const { spawn } = require("child_process");
 const assert = require("assert");
-const { version } = require("../package.json");
+const { version } = require("../../package.json");
 
 describe("Meta commands", () => {
   it("should return maru version", () => {
-    const prc = spawn("maru",  ["-v"]);
+    const prc = spawn("maru", ["-v"]);
     prc.stdout.setEncoding("utf8");
     prc.stdout.on("data", (data: string) => {
       const str = data.toString();
