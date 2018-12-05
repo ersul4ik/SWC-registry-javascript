2609154059f06d05abece265f3bf737fb3ec784e
thec00n <thec00n@fork.at>
2018-12-05 17:58:53 +0700

    refactor compilation

---
+++ src/utils/solc.ts
@@ -0,0 +1,75 @@
+import FileUtils from "./file";
+import logger from "../logger/logger";
+
+const detectInstalled = require('detect-installed')
+const niv = require('npm-install-version')
+
+class SolcUtility {
+
+    static isSolcVersionInstalled = async (version_number: string) => {
+        const version: string = "solc@" + version_number;
+        const result = (await detectInstalled(version, {
+            local: true
+        }))
+
+        return result;
+    }
+
+    static getPragmaVersion(file_name: string) {
+        // set a default version
+        let version = "0.4.25";
+        const file_content: string = FileUtils.getFileContent(file_name);
+        let arr = file_content.split('\n')
+
+        for (let x = 0; x < arr.length; x++) {
+            const filter_whitespaces: string = arr[x].replace(/\s|\t/g, "");
+            if (filter_whitespaces.startsWith("pragmasolidity")) {
+                // if a floating pragma is used then take the oldest allowed version
+                let extracted_version = filter_whitespaces.replace(/pragmasolidity|\^|;/g, "");
+
+                if (extracted_version.match(/^(\d+\.\d+\.\d+)$/)) {
+                    version = extracted_version;
+                } else {
+                    logger.error(`Extracting version failed in ${file_name} got ${extracted_version}`);
+                }
+            }
+        }
+
+        return version;
+    }
+
+    static compile(file_name: string) {
+
+        const version = SolcUtility.getPragmaVersion(file_name);
+        let compiler = undefined;
+
+        const solc_version_string: string = 'solc@' + version
+
+        if (SolcUtility.isSolcVersionInstalled(version)) {
+            logger.debug(`Solc version ${solc_version_string} not installed, trying to do that now.`)
+            niv.install(solc_version_string)
+        }
+
+        compiler = niv.require(solc_version_string)
+
+        let input = {
+            language: 'Solidity',
+            sources: {},
+            settings: {
+                outputSelection: {
+                    '*': {
+                        '': ['ast']
+                    }
+                }
+            }
+        }
+
+        const file_content: string = FileUtils.getFileContent(file_name);
+        input.sources = { [file_name]: { content: file_content } }
+
+        const out = JSON.parse(compiler.compileStandardWrapper(JSON.stringify(input)));
+        return out;
+    }
+}
+
+export default SolcUtility;
--- test/integration/compile.ts
+++ test/integration/compile.ts
@@ -1,12 +1,14 @@
 import AstUtility from "../../src/utils/ast";
+import SolcUtility from "../../src/utils/solc";
 
 const expect = require("expect");
-const remixLib = require('remix-lib')
-var fs = require('fs')
+const fs = require('fs')
 
 const niv = require('npm-install-version')
+
 niv.install('solc@0.4.24')
 const compiler = niv.require('solc@0.4.24')
+const detectInstalled = require('detect-installed')
 
 let input = {
     language: 'Solidity',
@@ -24,15 +26,25 @@ describe("Compile Sol File", () => {
     const file_name = "./test/sol_files/unary/typo_one_command.sol";
 
     it(`Extract sources from Solc AST for ${file_name}`, async () => {
-        var content = fs.readFileSync(file_name, 'utf8')
 
-        input.sources = { [file_name]: { content: content } }
-
-        const out = JSON.parse(compiler.compileStandardWrapper(JSON.stringify(input)));
+        const out = SolcUtility.compile(file_name);
 
         expect(Object.keys(out.sources)[0]).toEqual(file_name);
 
-        //  AstUtility.printNode(out)
+        // AstUtility.printNode(out)
     });
 
+    it(`Check if Solc version 0.4.24 is installed`, async () => {
+        niv.install('solc@0.4.24')
+
+        const result = await SolcUtility.isSolcVersionInstalled("0.4.24");
+        expect(result).toEqual(true);
+    });
+
+    it(`Extract pramga version from Sol file`, async () => {
+        const file_name = "./test/sol_files/unary/typo_one_command.sol";
+
+        const result = SolcUtility.getPragmaVersion(file_name)
+        expect(result).toEqual("0.4.24");
+    });
 });
