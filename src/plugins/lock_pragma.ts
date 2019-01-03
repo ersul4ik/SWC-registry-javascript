eaa36c3266d6ae77c2682be2b23171202f7dd8e4
thec00n <thec00n@fork.at>
2019-01-03 20:29:37 +0700

    add variant to floating pragma

--- src/plugins/lock_pragma.ts
+++ src/plugins/lock_pragma.ts
@@ -10,6 +10,7 @@ import Pragma from "../core/declarations/pragma";
 import PragmaUtils from "../utils/pragma";
 import Description from "../maru/description";
 import DescriptionUtils from "../utils/description";
+import Location from "../misc/location";
 
 let LockPragma: Plugin;
 
@@ -17,8 +18,10 @@ LockPragma = function(sol_file: SolFile, plugin_config: PluginConfig): IssuePoin
     const issuePointers: IssuePointer[] = [];
 
     for (const source of sol_file.sources) {
+        let version_found: boolean = false;
         for (const pragma of source.pragmas) {
             if (pragma.name.match("solidity")) {
+                version_found = true;
                 if (!PragmaUtils.isVersionFixed(pragma.value)) {
                     const formatted_description: Description = DescriptionUtils.formatParameters(plugin_config.description[0], [
                         PragmaUtils.getVersionWithoutCaret(pragma.value)
@@ -26,6 +29,15 @@ LockPragma = function(sol_file: SolFile, plugin_config: PluginConfig): IssuePoin
                     issuePointers.push(new IssuePointer(plugin_config.swcID, formatted_description, pragma.location));
                 }
             }
+        }
+        if (!version_found) {
+            issuePointers.push(
+                new IssuePointer(
+                    plugin_config.swcID,
+                    plugin_config.description[1],
+                    new Location(-1, sol_file.file_name, "0:0:0", 0, 0, 0, 0)
+                )
+            );
         }
     }
 
