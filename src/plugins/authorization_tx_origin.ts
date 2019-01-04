ba430d2e84c046a9505d73ade41f0b9c6e074122
thec00n <thec00n@fork.at>
2019-01-04 11:33:47 +0700

    refactor tx.origin plugin

--- src/plugins/authorization_tx_origin.ts
+++ src/plugins/authorization_tx_origin.ts
@@ -8,33 +8,41 @@ import SolFile from "../maru/sol_file";
 import PluginConfig from "../maru/plugin_config";
 import SolidityAntlr from "../parser/solidity_antlr";
 import Location from "../misc/location";
+import FunctionCall from "../core/expressions/function_call";
+import DescriptionUtils from "../utils/description";
+import MemberAccess from "../core/expressions/member_access";
+import NodeTypes from "../maru/node_types";
+import Identifier from "../core/expressions/identifier";
+import Description from "../maru/description";
 
 let AuthorizationTXOrigin: Plugin;
 AuthorizationTXOrigin = function(sol_file: SolFile, plugin_config: PluginConfig): IssuePointer[] {
     const issuePointers: IssuePointer[] = [];
-    /*
-    parser.visit(sol_file.block, {
-        BinaryOperation(b_op: any) {
-            if (NodeUtility.matchRegex(b_op.operator, new RegExp("==")) || NodeUtility.matchRegex(b_op.operator, new RegExp("!="))) {
-                parser.visit(b_op, {
-                    MemberAccess(member: any) {
-                        parser.visit(b_op, {
-                            Identifier(identifier: any) {
-                                if (
-                                    NodeUtility.matchRegex(member.memberName, new RegExp("^origin$")) &&
-                                    NodeUtility.matchRegex(identifier.name, new RegExp("^tx$"))
-                                ) {
-                                    const location: Location = SolidityAntlr.parseLocation(member.loc, member.range);
-                                    issuePointers.push(new IssuePointer(plugin_config.swcID, plugin_config.descriptionShort[0], location));
-                                }
-                            }
-                        });
+
+    for (const source of sol_file.sources) {
+        const members: MemberAccess[] = source.parseMemberAccess(source.source_unit[0].id);
+
+        for (const member of members) {
+            const identifier: Identifier = source.parseIdentifiers(member.location.id)[0];
+
+            if (
+                NodeUtility.matchRegex(member.member_name, new RegExp("^origin$")) &&
+                NodeUtility.matchRegex(identifier.name, new RegExp("^tx$"))
+            ) {
+                let description: Description = plugin_config.description[0];
+                let parents: any[] = source.getParents(member.location.id);
+
+                for (const parent of parents) {
+                    if (NodeUtility.matchString(parent.name, NodeTypes.BinaryOperation)) {
+                        description = plugin_config.description[1];
                     }
-                });
+                }
+
+                issuePointers.push(new IssuePointer(plugin_config.swcID, description, member.location));
             }
         }
-    });
-    */
+    }
+
     return issuePointers;
 };
 
