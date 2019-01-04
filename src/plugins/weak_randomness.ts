b11b837d8f64206857cdfdfbc3df1625a2402046
thec00n <thec00n@fork.at>
2019-01-04 12:02:19 +0700

    refactor weak randomness

--- src/plugins/weak_randomness.ts
+++ src/plugins/weak_randomness.ts
@@ -6,25 +6,35 @@ import SolFile from "../maru/sol_file";
 import PluginConfig from "../maru/plugin_config";
 import FunctionCall from "../core/expressions/function_call";
 import SolidityAntlr from "../parser/solidity_antlr";
+import NodeUtility from "../utils/node";
+import Description from "../maru/description";
+import DescriptionUtils from "../utils/description";
 
 let WeakRandomnessFunction: Plugin;
 
 WeakRandomnessFunction = function(sol_file: SolFile, plugin_config: PluginConfig): IssuePointer[] {
     const issuePointers: IssuePointer[] = [];
 
-    /*
-    for (const c of sol_file.contracts_current) {
-        const f_cs: FunctionCall[] = SolidityAntlr.parseFunctionCalls(c.subNodes);
+    for (const source of sol_file.sources) {
+        const f_cs: FunctionCall[] = source.parseFunctionCalls(source.source_unit[0].id);
 
         for (const f_c of f_cs) {
-            if (NodeUtility.matchRegex(f_c.name, new RegExp("^block.blockhash$"))) {
-                issuePointers.push(new IssuePointer(plugin_config.swcID, plugin_config.descriptionShort[0], f_c.location));
-            } else if (NodeUtility.matchRegex(f_c.name, new RegExp("^blockhash$"))) {
-                issuePointers.push(new IssuePointer(plugin_config.swcID, plugin_config.descriptionShort[0], f_c.location));
+            if (
+                NodeUtility.matchRegex(f_c.member_name, new RegExp("^blockhash$")) &&
+                NodeUtility.matchRegex(f_c.identifier_name, new RegExp("^block$"))
+            ) {
+                const formatted_description: Description = DescriptionUtils.formatParameters(plugin_config.description[0], [
+                    "block.blockhash"
+                ]);
+                issuePointers.push(new IssuePointer(plugin_config.swcID, formatted_description, f_c.location));
+            } else if (NodeUtility.matchRegex(f_c.identifier_name, new RegExp("^blockhash$"))) {
+                const formatted_description: Description = DescriptionUtils.formatParameters(plugin_config.description[0], ["blockhash"]);
+
+                issuePointers.push(new IssuePointer(plugin_config.swcID, formatted_description, f_c.location));
             }
         }
     }
-*/
+
     return issuePointers;
 };
 
