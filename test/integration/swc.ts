0eb86f368a3ec6267dc2cde76644bac5d0b54699
thec00n <thec00n@fork.at>
2019-01-15 09:46:18 +0700

    run all tests

--- test/integration/swc.ts
+++ test/integration/swc.ts
@@ -10,19 +10,19 @@ describe("Run Maru against all files in the SWC-registry", () => {
     const directory: string = "./test/SWC-registry/test_cases/";
     const pattern: string = ".sol";
     const file_names = FileUtils.searchRecursive(directory, pattern);
-    //  for (const file_name of file_names[0]) {
-    it(`Test Cases: should return MythX formatted JSON output and no errors for ${file_names[0]}`, () => {
-        const prc = spawnSync("maru", ["-r", file_names[0], "-o", "json"]);
-        let reports: Report[] = JSON.parse(prc.stdout.toString());
+    for (const file_name of file_names[0]) {
+        it(`Test Cases: should return MythX formatted JSON output and no errors for ${file_name}`, () => {
+            const prc = spawnSync("maru", ["-r", file_name, "-o", "json"]);
+            let reports: Report[] = JSON.parse(prc.stdout.toString());
 
-        expect(reports.length).toEqual(1);
-        expect(reports[0].issues.length).toBeGreaterThanOrEqual(0);
-        expect(reports[0].sourceType).toEqual("solidity-file");
-        expect(reports[0].sourceFormat).toEqual("text");
-        expect(reports[0].sourceList.length).toBeGreaterThanOrEqual(1);
-        expect(reports[0].meta.selected_compiler.length).toBeGreaterThan(0);
-        expect(reports[0].meta.error.length).toEqual(0);
-        expect(reports[0].meta.error.length).toBeGreaterThanOrEqual(0);
-    });
-    // }
+            expect(reports.length).toEqual(1);
+            expect(reports[0].issues.length).toBeGreaterThanOrEqual(0);
+            expect(reports[0].sourceType).toEqual("solidity-file");
+            expect(reports[0].sourceFormat).toEqual("text");
+            expect(reports[0].sourceList.length).toBeGreaterThanOrEqual(1);
+            expect(reports[0].meta.selected_compiler.length).toBeGreaterThan(0);
+            expect(reports[0].meta.error.length).toEqual(0);
+            expect(reports[0].meta.error.length).toBeGreaterThanOrEqual(0);
+        });
+    }
 });
